68f8ccd87a629ee67da6e80222a07bf44de014ad
RAHULRAANU <rahulkrprasad18@gmail.com>
2023-12-28 17:37:23 +0530

    upd

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 7777 || 3000, // Use dynamic port or fallback to 3000
-  },
-});
+})
+
